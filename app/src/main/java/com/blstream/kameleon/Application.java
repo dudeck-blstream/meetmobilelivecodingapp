63cc0d7fdb8278d7b6cf8494a45338352792577b
grdziubi <kontakt@dziubinski.eu>
2016-03-11 14:40:47 +0100

    Files

---
+++ app/src/main/java/com/blstream/kameleon/Application.java
@@ -0,0 +1,16 @@
+package com.blstream.kameleon;
+
+import com.estimote.sdk.EstimoteSDK;
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //  App ID & App Token can be taken from App section of Estimote Cloud.
+        EstimoteSDK.initialize(getApplicationContext(), "cameleon-5vf", "b706e95f843b8b10b8f19a82f69c48e2");
+
+        // Optional, debug logging.
+        EstimoteSDK.enableDebugLogging(true);
+    }
+}
