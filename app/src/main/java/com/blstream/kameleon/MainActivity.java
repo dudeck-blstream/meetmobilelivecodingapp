d217dd364057aa60328d8f8f917701998eefaff7
GrzegorzDec <grzegorz.dec@blstream.com>
2016-03-11 13:51:29 +0100

    asd

---
+++ app/src/main/java/com/blstream/kameleon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.blstream.kameleon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
